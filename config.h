a2bb2e6f80831ceffb85efd85fb5b4a5a7287a1c
Emil Engler <engler+git@unveil2.org>
2022-05-02 21:49:06 +0200

    config: Add Linux compatability layer
    This commit adds a config.h file, which defines several pre-processor
    contants and includes a file for BSD functions, if the user is masochist
    and decides to use Linux.

--- config.h
+++ config.h
@@ -14,45 +14,17 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include "config.h"
+#ifndef CONFIG_H
+#define CONFIG_H
 
-#include <sys/types.h>
+#ifdef __linux__
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
+#define _DEFAULT_SOURCE
 
-#include "util.h"
+#include <bsd/bsd.h>
 
-/*
- * Return an allocated string in the printf(3) style.
- * It must be passed to free(3) afterwards.
- * See asprintf(3).
- */
-char *
-util_aprintf(const char *fmt, ...)
-{
-	char	*s;
-	va_list	 v1, v2;
-	int	 sz;
+#define INFTIM	-1
 
-	s = NULL;
-	va_start(v1, fmt);
-	va_copy(v2, v1);
+#endif
 
-	sz = vsnprintf(NULL, 0, fmt, v1);
-	if (sz < 0)
-		goto end;
-
-	if ((s = malloc(sz + 1)) == NULL)
-		goto end;
-
-	if (vsnprintf(s, sz + 1, fmt, v2) < 0) {
-		free(s);
-		s = NULL;
-	}
-end:
-	va_end(v1);
-	va_end(v2);
-	return (s);
-}
+#endif
