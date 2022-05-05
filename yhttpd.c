19bcaed703d0c8fc4b87b9579c89f0bad972e1bc
Emil Engler <engler+git@emux.org>
2022-05-05 12:49:20 +0200

    yhttpd: Foundation for the web server

--- yhttpd.c
+++ yhttpd.c
@@ -14,8 +14,96 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-int
-main(int argc, const char *argv[])
+#include <sys/types.h>
+
+#include <err.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "yhttp.h"
+
+static struct yhttp	*yh = NULL;
+
+static void		parse_args(int, char *[]);
+static void		sighdlr(int);
+static __dead void	usage(void);
+static void		yhttp_cb(struct yhttp_requ *, void *);
+
+static int		 virtual_hosts = 0;
+static uint16_t		 port = 8080;
+
+static void
+parse_args(int argc, char *argv[])
 {
+	int	ch, port_arg;
+
+	/* Parse the command line arguments. */
+	while ((ch = getopt(argc, argv, "hp:")) != -1) {
+		switch (ch) {
+		case 'h':
+			virtual_hosts = 1;
+			break;
+		case 'p':
+			port_arg = atoi(optarg);
+			if (port_arg < 1024)
+				errx(1, "port must be > 1023");
+			else if (port_arg > 65535)
+				errx(1, "port must be < 65536");
+			port = (uint16_t) port_arg;
+			break;
+		default:
+			usage();
+		}
+	}
+
+	if (optind == argc) {
+		/* DIRECTORY not supplied. */
+		usage();
+	}
+}
+
+static void
+sighdlr(int sig)
+{
+	int	rc;
+
+	fprintf(stderr, "Received signal %d, exiting...\n", sig);
+	if ((rc = yhttp_stop(yh)) != YHTTP_OK)
+		errx(1, "yhttp_stop: %d\n", rc);
+}
+
+static __dead void
+usage(void)
+{
+	errx(1, "[-h] [-p PORT] DIRECTORY");
+}
+
+static void
+yhttp_cb(struct yhttp_requ *requ, void *udata)
+{
+}
+
+int
+main(int argc, char *argv[])
+{
+	int	rc;
+
+	parse_args(argc, argv);
+
+	signal(SIGINT, sighdlr);
+	signal(SIGTERM, sighdlr);
+
+	if ((yh = yhttp_init(port)) == NULL)
+		err(1, "yhttp_init");
+
+	fprintf(stderr, "Listening on TCP port %d\n", port);
+	if ((rc = yhttp_dispatch(yh, yhttp_cb, NULL)) != YHTTP_OK)
+		errx(1, "yhttp_dispatch: %d\n", rc);
+
+	yhttp_free(&yh);
+
 	return (0);
 }
