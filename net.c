emilengler/yhttp

commit 794dc0d1de33a489f47659992493a7e8a220c22f

net: Fix segmentation fault
This commit fixes a segmentation fault introduced by 1c1fff0, which
occurred when the resp() or resp_err() function failed, because the
net_handle_client() function then closed the connection but continued
the execution, meaning further use of free'd data, resulting in a
segmentation fault.

net.c
@@ -130,8 +130,11 @@ net_handle_client(struct poll_data *pd, size_t index,
 		}
 
 		if (pd->parsers[index]->err) {
-			if (resp_err(s, pd->parsers[index]->err) != YHTTP_OK)
+			rc = resp_err(s, pd->parsers[index]->err);
+			if (rc != YHTTP_OK) {
 				net_poll_close(pd, index);
+				return (rc);
+			}
 			return (net_finish_requ(pd, index));
 		} else if (pd->parsers[index]->state == PARSER_DONE) {
 			/* Obtain the IP address. */
@@ -141,8 +144,11 @@ net_handle_client(struct poll_data *pd, size_t index,
 
 			internal = pd->parsers[index]->requ->internal;
 			cb(pd->parsers[index]->requ, udata);
-			if (resp(s, internal->resp) != YHTTP_OK)
+			rc = resp(s, internal->resp);
+			if (rc != YHTTP_OK) {
 				net_poll_close(pd, index);
+				return (rc);
+			}
 			return (net_finish_requ(pd, index));
 		}
 	}

commit acd2f00befb90a47c005c0f037554bc7db250a86

net: Return with YHTTP_OK if the response failed
This commit fixes a critical vulnerability introduced by 794dc0d, namely
an exploit that would have allowed an attacker to terminate the
execution of the server.

The reason for this was, that the net_handle_client() function would
have returned with the error code of resp() and resp_err() and not
YHTTP_OK, resulting in net_dispatch() and yhttp_dispatch() to fail with
that error code as well.  Because these two functions return an error
if the transmission has failed, an attacker could have simply triggered
such an error by terminating her connection during a transfer.

net.c
@@ -130,10 +130,9 @@ net_handle_client(struct poll_data *pd, size_t index,
 		}
 
 		if (pd->parsers[index]->err) {
-			rc = resp_err(s, pd->parsers[index]->err);
-			if (rc != YHTTP_OK) {
+			if (resp_err(s, pd->parsers[index]->err) != YHTTP_OK) {
 				net_poll_close(pd, index);
-				return (rc);
+				return (YHTTP_OK);
 			}
 			return (net_finish_requ(pd, index));
 		} else if (pd->parsers[index]->state == PARSER_DONE) {
@@ -144,10 +143,9 @@ net_handle_client(struct poll_data *pd, size_t index,
 
 			internal = pd->parsers[index]->requ->internal;
 			cb(pd->parsers[index]->requ, udata);
-			rc = resp(s, internal->resp);
-			if (rc != YHTTP_OK) {
+			if (resp(s, internal->resp) != YHTTP_OK) {
 				net_poll_close(pd, index);
-				return (rc);
+				return (YHTTP_OK);
 			}
 			return (net_finish_requ(pd, index));
 		}