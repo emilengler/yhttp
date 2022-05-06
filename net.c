emilengler/yhttp

commit 1c1fff04189303907f6fa58a3a259209ae080bc9

net: Respect the "Connection" header field
This commit implements the net_finish_requ() function, a function
responsible for closing or resetting finished requests.

What this function does, is utilizing the net_is_keep_alive() function,
introduced by 62ccee8, and reset the parser field if true or call
net_poll_close() if false.

net.c
@@ -51,6 +51,8 @@ struct poll_data {
 	size_t		  used;
 };
 
+static int	net_finish_requ(struct poll_data *, size_t);
+
 static int	net_handle_accept(struct poll_data *, size_t);
 static int	net_handle_client(struct poll_data *, size_t,
 				  void (*)(struct yhttp_requ *, void *),
@@ -64,6 +66,23 @@ static int	net_poll_add(struct poll_data *, int, short);
 static void	net_poll_del(struct poll_data *, size_t);
 static void	net_poll_close(struct poll_data *, size_t);
 static int	net_socket(int, uint16_t);
+
+static int
+net_finish_requ(struct poll_data *pd, size_t index)
+{
+	if (net_is_keep_alive(pd->parsers[index]->requ)) {
+		/* Connection is keep-alive, just reset it. */
+		parser_free(pd->parsers[index]);
+
+		if ((pd->parsers[index] = parser_init()) == NULL)
+			return (YHTTP_ERRNO);
+		return (YHTTP_OK);
+	} else {
+		/* Connection is close, close it. */
+		net_poll_close(pd, index);
+		return (YHTTP_OK);
+	}
+}
 
 static int
 net_handle_accept(struct poll_data *pd, size_t index)
@@ -110,13 +129,13 @@ net_handle_client(struct poll_data *pd, size_t index,
 		if (pd->parsers[index]->err) {
 			if (resp_err(s, pd->parsers[index]->err) != YHTTP_OK)
 				net_poll_close(pd, index);
-			/* TODO: Respect the Connection header field. */
+			return (net_finish_requ(pd, index));
 		} else if (pd->parsers[index]->state == PARSER_DONE) {
 			internal = pd->parsers[index]->requ->internal;
 			cb(pd->parsers[index]->requ, udata);
 			if (resp(s, internal->resp) != YHTTP_OK)
 				net_poll_close(pd, index);
-			/* TODO: Respect the Connection header field. */
+			return (net_finish_requ(pd, index));
 		}
 	}
 

commit 794dc0d1de33a489f47659992493a7e8a220c22f

net: Fix segmentation fault
This commit fixes a segmentation fault introduced by 1c1fff0, which
occurred when the resp() or resp_err() function failed, because the
net_handle_client() function then closed the connection but continued
the execution, meaning further use of free'd data, resulting in a
segmentation fault.

net.c
@@ -130,8 +130,11 @@ net_handle_client(struct poll_data *pd, size_t index,
 		}
 
 		if (pd->parsers[index]->err) {
-			if (resp_err(s, pd->parsers[index]->err) != YHTTP_OK)
+			rc = resp_err(s, pd->parsers[index]->err);
+			if (rc != YHTTP_OK) {
 				net_poll_close(pd, index);
+				return (rc);
+			}
 			return (net_finish_requ(pd, index));
 		} else if (pd->parsers[index]->state == PARSER_DONE) {
 			/* Obtain the IP address. */
@@ -141,8 +144,11 @@ net_handle_client(struct poll_data *pd, size_t index,
 
 			internal = pd->parsers[index]->requ->internal;
 			cb(pd->parsers[index]->requ, udata);
-			if (resp(s, internal->resp) != YHTTP_OK)
+			rc = resp(s, internal->resp);
+			if (rc != YHTTP_OK) {
 				net_poll_close(pd, index);
+				return (rc);
+			}
 			return (net_finish_requ(pd, index));
 		}
 	}